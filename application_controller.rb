21a75d8630511837d047f63745ee8340b7071c4d
xgumball <anthonyviana4241997@gmail.com>
2016-01-29 05:21:42 +0000

    DOne

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,15 @@
 require 'bundler'
 Bundler.require
-
+require_relative 'models/pig_latinize.rb'
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    erb :index
+  get '/' do #this is the name of the route
+      erb :index
   end
-
+  
+  post '/results' do
+        sentence = params[:sentence]
+      @pig_latinized_sentence = to_pig_latin(sentence)
+        erb :results
+  end
 end
